2dc5f2afba87bca2d726841236decc0d5afbbf4f
Ehtisham <ehtisham.ahmad89@gmail.com>
2024-06-06 11:23:57 +0300

    Megred with upgrade-graphql-7.7.1

--- GQLTypes/EmployeeType.cs
+++ GQLTypes/EmployeeType.cs
@@ -5,14 +5,11 @@ namespace RestApiGraphQL.GQLTypes
     public class EmployeeType: ObjectGraphType<EmpTbl>
     {
         public EmployeeType() {
-            Models.Invoices.RestApiGraphQldbContext invoices = new Models.Invoices.RestApiGraphQldbContext();
-
             Field(emp => emp.Id);
             Field(emp => emp.Age);
             Field(emp => emp.City);
             Field(emp => emp.Name);
             Field(emp => emp.Email);
-            Field<InvoicesType>("dept", resolve: context => invoices.InvoicesTbls.SingleOrDefault(context.Source.Age < 50));
         }
     }
 }
